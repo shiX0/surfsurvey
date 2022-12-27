c31719974c7f721ea3bb7beb9d14680ab6ec60ac
SuyogManandhar <skmanandhar803@gmail.com>
2022-12-27 13:29:15 +0545

    name change create survey

--- src/view/create_survey.java
+++ src/view/create_survey.java
@@ -9,13 +9,13 @@ import database.Dbconnection;
  *
  * @author EGT
  */
-public class myframe extends javax.swing.JFrame {
+public class create_survey extends javax.swing.JFrame {
 
   
     /**
      * Creates new form myframe
      */
-    public myframe() {
+    public create_survey() {
         initComponents();
         jTextField2.setVisible(false);
         jTextField3.setVisible(false);
@@ -303,20 +303,21 @@ public class myframe extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(myframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(create_survey.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(myframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(create_survey.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(myframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(create_survey.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(myframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(create_survey.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new myframe().setVisible(true);
+                new create_survey().setVisible(true);
             }
         });
     }
